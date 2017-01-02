d582425d096a77d037603a0a76d44b528a323708
ALobker <ALobker@users.noreply.github.com>
2017-01-02 12:38:19 +0100

    Update clock.js
    Fixed a bug where the 10 minutes and 10 hours segments would show incorrectly.

--- example/clock.js
+++ example/clock.js
@@ -10,11 +10,11 @@ var interval = setInterval( function() {
       minute = date.getMinutes();
 
   //Hours
-  display.writeDigit(0, Math.round(hour / 10));
+  display.writeDigit(0, Math.floor(hour / 10));
     display.writeDigit(1, hour % 10);
 
     //Minutes
-    display.writeDigit(3, Math.round(minute / 10));
+    display.writeDigit(3, Math.floor(minute / 10));
     display.writeDigit(4, minute % 10);
 
     //Colon
